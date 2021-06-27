ad7e08c0fd1e0b64fe01d91884d959d181b0bbf8
mdarif6 <md1arif015@gmail.com>
2021-06-27 18:13:13 +0530

    Even/Odd Commit
    Even/Odd Commit

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ if (random < 0.5) {
   console.log("Your Number is Less than 0.5");
   console.log(random);
 }
+
+if (random > 0.5) {
+  console.log("Your Number is greater than 0.5");
+  console.log(random);
+}
+
+function isEven(num) {
+  if (num % 2 == 0) {
+    console.log("Number is Even");
+  } else {
+    console.log("Number is odd");
+  }
+}
+isEven(7);
